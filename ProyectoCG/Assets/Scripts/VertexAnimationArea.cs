f41b9eaa05f0b499dc7a27c0347f141f407c0c49
BLE4REAL <arajuanes405@gmail.com>
2026-03-05 16:41:49 -0500

    a
    a

--- ProyectoCG/Assets/Scripts/VertexAnimationArea.cs
+++ ProyectoCG/Assets/Scripts/VertexAnimationArea.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    SerializeField private Material targetMaterial;
-
-    SerializeField private float radius;
+    [SerializeField] private Material targetMaterial;
+    [SerializeField] private float radius;
 
     void Update()
     {
         Vector3 pos = transform.position;
-        Vector4 sphere = Vector4(pos.x, pos.y, pos.z, w: radius);
-        targetMaterial.SetVector(name: "_Sphere_Descriptor", sphere);
+        Vector4 sphere = new Vector4(pos.x, pos.y, pos.z, radius);
+        targetMaterial.SetVector("_Sphere_Descriptor", sphere);
+    } 
+
+    void OnDrawGizmos()
+   {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
+
 }
